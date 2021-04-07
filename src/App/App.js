5db156a1395d7352a84f35caa0b05227e62e0835
Max Bregman <bregmanmax91@gmail.com>
2021-04-07 11:15:24 -0600

    Could not find cynamic way for useStae calls in change state on chane, was able to make it work with if statements

--- src/App/App.js
+++ src/App/App.js
@@ -5,6 +5,10 @@ function App () {
 
 
   const [reservations, setReservations] = useState()
+  const [name, setName] = useState()
+  const [date, setDate] = useState()
+  const [time, setTime] = useState()
+  const [number, setNumber] = useState()
 
   useEffect(() => {
     fetch('http://localhost:3001/api/v1/reservations')
@@ -12,12 +16,65 @@ function App () {
     .then(data => setReservations(data))
   },[])
 
+
+  function handleChange(event) {
+   
+    if (event.target.name === 'name') {
+      setName(event.target.value)
+    }
+    if (event.target.name === 'date') {
+      setDate(event.target.value)
+    }
+    if (event.target.name === 'time') {
+      setTime(event.target.value)
+    }
+    if (event.target.name === 'number') {
+      setNumber(event.target.value)
+    }
+  }
   
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          <form>
+            <label>
+              Name
+              <input
+                name='name'
+                type='text'
+                value={name}
+                onChange={event => handleChange(event)}
+              />
+            </label>
+            <label>
+              Date
+              <input
+                name='date'
+                type='text'
+                value={date}
+                onChange={event => handleChange(event)}
+              />
+            </label>
+            <label>
+              Time
+              <input
+                name='time'
+                type='text'
+                value={time}
+                onChange={event => handleChange(event)}
+              />
+            </label>
+            <label>
+              Number
+              <input
+                name='number'
+                type='text'
+                value={number}
+                onChange={event => handleChange(event)}
+              />
+            </label>
+          </form>
         </div>
         <div className='resy-container'>
           {reservations && <section>{reservations.map(reservation => {
